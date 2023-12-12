ce3520abfc20d5d36e00306edafa65e06da96a69
zs <978007000@qq.com>
2023-12-12 11:48:40 +0800

    修正：防止绑定中的无限递归。

--- app/server/binding/internal/decoder/decoder.go
+++ app/server/binding/internal/decoder/decoder.go
@@ -45,7 +45,9 @@ func GetReqDecoder(rt reflect.Type, byTag string, config *DecodeConfig) (Decoder
 			continue
 		}
 
-		dec, needValidate2, err := getFieldDecoder(el.Field(i), i, []int{}, "", byTag, config)
+		// dec, needValidate2, err := getFieldDecoder(el.Field(i), i, []int{}, "", byTag, config)
+		dec, needValidate2, err := getFieldDecoder(parentInfos{[]reflect.Type{el}, []int{}, ""}, el.Field(i), i, byTag, config)
+
 		if err != nil {
 			return nil, false, err
 		}
@@ -68,7 +70,14 @@ func GetReqDecoder(rt reflect.Type, byTag string, config *DecodeConfig) (Decoder
 	}, needValidate, nil
 }
 
-func getFieldDecoder(field reflect.StructField, index int, parentIdx []int, parentJSONName, byTag string, config *DecodeConfig) ([]fieldDecoder, bool, error) {
+type parentInfos struct {
+	Types    []reflect.Type
+	Indexes  []int
+	JSONName string
+}
+
+// func getFieldDecoder(field reflect.StructField, index int, parentIdx []int, parentJSONName, byTag string, config *DecodeConfig) ([]fieldDecoder, bool, error) {
+func getFieldDecoder(pInfo parentInfos, field reflect.StructField, index int, byTag string, config *DecodeConfig) ([]fieldDecoder, bool, error) {
 	for field.Type.Kind() == reflect.Ptr {
 		field.Type = field.Type.Elem()
 	}
@@ -81,7 +90,7 @@ func getFieldDecoder(field reflect.StructField, index int, parentIdx []int, pare
 	}
 
 	// 形如 'a.b.c' 的 JSONName 用于必填验证。
-	fieldTagInfos, newParentJSONName, needValidate := lookupFieldTags(field, parentJSONName, config)
+	fieldTagInfos, newParentJSONName, needValidate := lookupFieldTags(field, pInfo.JSONName, config)
 	if len(fieldTagInfos) == 0 && !config.DisableDefaultTag {
 		fieldTagInfos = getDefaultFieldTags(field)
 	}
@@ -91,19 +100,19 @@ func getFieldDecoder(field reflect.StructField, index int, parentIdx []int, pare
 
 	// 自定义类型解码器拥有最高优先级
 	if customizedFunc, exists := config.TypeUnmarshalFuncs[field.Type]; exists {
-		dec, err := getCustomizedFieldDecoder(field, index, fieldTagInfos, parentIdx, customizedFunc, config)
+		dec, err := getCustomizedFieldDecoder(field, index, fieldTagInfos, pInfo.Indexes, customizedFunc, config)
 		return dec, needValidate, err
 	}
 
 	// 切片、数组字段解码器
 	if field.Type.Kind() == reflect.Slice || field.Type.Kind() == reflect.Array {
-		dec, err := getSliceFieldDecoder(field, index, fieldTagInfos, parentIdx, config)
+		dec, err := getSliceFieldDecoder(field, index, fieldTagInfos, pInfo.Indexes, config)
 		return dec, needValidate, err
 	}
 
 	// 映射字段解码器
 	if field.Type.Kind() == reflect.Map {
-		dec, err := getMapFieldDecoder(field, index, fieldTagInfos, parentIdx, config)
+		dec, err := getMapFieldDecoder(field, index, fieldTagInfos, pInfo.Indexes, config)
 		return dec, needValidate, err
 	}
 
@@ -114,12 +123,11 @@ func getFieldDecoder(field reflect.StructField, index int, parentIdx []int, pare
 		// todo 更多内置常用结构体绑定，如时间...
 		switch el {
 		case reflect.TypeOf(multipart.FileHeader{}):
-			dec, err := getMultipartFileDecoder(field, index, fieldTagInfos, parentIdx, config)
+			dec, err := getMultipartFileDecoder(field, index, fieldTagInfos, pInfo.Indexes, config)
 			return dec, needValidate, err
 		}
-
 		if !config.DisableStructFieldResolve { // 单独解码结构体类型
-			structFieldDecoder, err := getStructTypeFieldDecoder(field, index, fieldTagInfos, parentIdx, config)
+			structFieldDecoder, err := getStructTypeFieldDecoder(field, index, fieldTagInfos, pInfo.Indexes, config)
 			if err != nil {
 				return nil, needValidate, err
 			}
@@ -127,17 +135,27 @@ func getFieldDecoder(field reflect.StructField, index int, parentIdx []int, pare
 				decoders = append(decoders, structFieldDecoder...)
 			}
 		}
+
+		// 防止无限递归：结构体的字段类型与父结构体相同时会产生。
+		if hasSameType(pInfo.Types, el) {
+			return decoders, needValidate, nil
+		}
+
+		pIdx := pInfo.Indexes
 		for i := 0; i < el.NumField(); i++ {
 			if el.Field(i).PkgPath != "" && !el.Field(i).Anonymous {
 				// 忽略未导出字段
 				continue
 			}
 			var indices []int
-			if len(parentIdx) > 0 {
-				indices = append(indices, parentIdx...)
+			if len(pInfo.Indexes) > 0 {
+				indices = append(indices, pIdx...)
 			}
 			indices = append(indices, index)
-			dec, needValidate2, err := getFieldDecoder(el.Field(i), i, indices, newParentJSONName, byTag, config)
+			pInfo.Indexes = indices
+			pInfo.Types = append(pInfo.Types, el)
+			pInfo.JSONName = newParentJSONName
+			dec, needValidate2, err := getFieldDecoder(pInfo, el.Field(i), i, byTag, config)
 			needValidate = needValidate || needValidate2
 			if err != nil {
 				return nil, false, err
@@ -150,6 +168,16 @@ func getFieldDecoder(field reflect.StructField, index int, parentIdx []int, pare
 	}
 
 	// 基本类型解码器
-	dec, err := getBaseTypeTextDecoder(field, index, fieldTagInfos, parentIdx, config)
+	dec, err := getBaseTypeTextDecoder(field, index, fieldTagInfos, pInfo.Indexes, config)
 	return dec, needValidate, err
+}
+
+// hasSameType 确定父子关系中是否存在相同类型
+func hasSameType(pts []reflect.Type, ft reflect.Type) bool {
+	for _, pt := range pts {
+		if reflect.DeepEqual(getElemType(pt), getElemType(ft)) {
+			return true
+		}
+	}
+	return false
 }
